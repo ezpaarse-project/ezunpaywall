fc4833fd6ab90e57c37edf27bddbc40d9cc55d9f
felixleo22 <leofelixoff@outlook.fr>
2020-07-07 13:54:19 +0200

    update log

--- api/routers/manageDatabase.js
+++ api/routers/manageDatabase.js
@@ -5,9 +5,7 @@ const zlib = require('zlib');
 const UnPayWallModel = require('../graphql/unpaywall/model');
 const logger = require('../../logs/logger');
 
-function saveDataOrUpdate(file) {
-  let counterLimit = 10000;
-  const nbtraitement = counterLimit;
+function saveDataOrUpdate(file, offset, limit) {
   let counterLine = 0;
   // UnPayWall attributes
   const metadata = [
@@ -32,77 +30,94 @@ function saveDataOrUpdate(file) {
     'z_authors',
     'createdAt',
   ];
-  // create and update
+  // upsert
   function updateUPW(data) {
+    logger.info(`${counterLine}th Lines processed`);
     UnPayWallModel.bulkCreate(data, { updateOnDuplicate: metadata })
       .catch((error) => {
-        logger.error(`BULK ERROR : ${error}`);
+        logger.error(`ERROR IN UPSERT : ${error}`);
       });
   }
-
   // stream initialization
   const readStream = fs.createReadStream(file).pipe(zlib.createGunzip());
-
   async function processLineByLineUpdate() {
     const start = new Date();
-
     let tab = [];
     let countBulk = 0;
-
     const rl = readline.createInterface({
       input: readStream,
       crlfDelay: Infinity,
     });
     // eslint-disable-next-line no-restricted-syntax
     for await (const line of rl) {
-      if (counterLimit === 0) {
+      // test limit
+      if (counterLine === limit) {
         break;
       }
+      // test offset
+      if (counterLine >= offset) {
+        countBulk += 1;
+        const data = JSON.parse(line);
+        tab.push(data);
+      }
       counterLine += 1;
-      const data = JSON.parse(line);
-      tab.push(data);
-      countBulk += 1;
-      if (countBulk === 1000) {
+      if ((countBulk % 1000) === 0 && countBulk !== 0) {
         await updateUPW(tab);
-        countBulk = 0;
         tab = [];
       }
-      counterLimit -= 1;
     }
     // if have stays data to insert
-    if (countBulk !== 0) {
+    if (countBulk > 0) {
       await updateUPW(tab);
     }
 
     const total = (new Date() - start) / 1000;
-    logger.info(`${total} secondes`);
-    logger.info(`Nombre d'erreur : ${counterLine - nbtraitement}`);
+    logger.info('============= FINISH =============');
+    logger.info(`${total} seconds`);
+    logger.info(`Number of treated lines : ${countBulk}`);
+    logger.info(`Number of errors : ${counterLine - (countBulk + offset)}`);
   }
   processLineByLineUpdate();
 }
+
 /**
  * @api {get} /firstInitialization initialize database with a compressed file
  * @apiName InitiateDatabaseWithCompressedFile
  * @apiGroup ManageDatabase
+ *
+ * @apiParam (QUERY) {Number} [offset=0] first line insertion, by default, we start with the first
+ * @apiParam (QUERY) {Number} [limit=0] last line insertion by default, we have no limit
  */
 router.get('/firstInitialization', async (req, res) => {
+  let { offset, limit } = req.query;
+  if (!offset) { offset = 0; }
+  if (!limit) { limit = -1; }
   const file = './dataUPW/unpaywall_snapshot.jsonl.gz';
-  saveDataOrUpdate(file);
+  saveDataOrUpdate(file, Number(offset), Number(limit));
   res.json({ name: 'first initialization with file compressed' });
 });
 
-// TODO voir pour automatisé le lancement de ce script avec un
-// téléchargement automatique des mise à jour
+
 /**
+ * // TODO voir pour automatisé le lancement de ce script avec un
+ * téléchargement automatique des mise à jour
  * @api {get} /updateDatabase update database with weekly snapshot
  * @apiName UpdateDatabaseWithCompressedFile
  * @apiGroup ManageDatabase
+ *
+ * @apiParam (QUERY) {Number} [offset=0] first line insertion, by default, we start with the first
+ * @apiParam (QUERY) {Number} [limit=0] last line insertion by default, we have no limit
+ *
  */
 router.get('/updateDatabase', (req, res) => {
+  let { offset, limit } = req.query;
+  if (!offset) { offset = 0; }
+  if (!limit) { limit = -1; }
   const file = './dataUPW/update.gz';
-  saveDataOrUpdate(file);
+  saveDataOrUpdate(file, Number(offset), Number(limit));
   res.json({ name: 'update database ...' });
 });
+
 /**
  * @api {get} /databaseStatus get informations of content database
  * @apiName GetDatabaseStatus
@@ -152,7 +167,7 @@ router.get('/databaseStatus', (req, res) => {
         oa_status: 'closed',
       },
     });
-    logger.info(`Etat de la base - doi:${status.doi}, is_oa ${status.is_oa}, journal_issn_l: ${status.journal_issn_l}, publisher: ${status.publisher}, gold: ${status.gold}, hybrid: ${status.hybrid}, bronze: ${status.bronze}, green: ${status.green}, closed: ${status.closed}`);
+    logger.info(`Databse status - doi:${status.doi}, is_oa ${status.is_oa}, journal_issn_l: ${status.journal_issn_l}, publisher: ${status.publisher}, gold: ${status.gold}, hybrid: ${status.hybrid}, bronze: ${status.bronze}, green: ${status.green}, closed: ${status.closed}`);
     res.json({
       doi: status.doi,
       is_oa: status.is_oa,
