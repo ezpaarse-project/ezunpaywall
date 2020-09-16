a7e6d6805f76252150d4f805df3b6bd2ded44527
felixleo22 <leofelixoff@outlook.fr>
2020-09-16 11:17:52 +0200

    update docker-compose with elasticsearch

--- test/testApi.js
+++ test/testApi.js
@@ -6,24 +6,23 @@ const server = require('../app');
 chai.should();
 chai.use(chaiHttp);
 
-const dataTest1 = require('./dataTest1.json');
-const dataTest2 = require('./dataTest2.json');
+const doi1 = '10.1901/jaba.2006.153-04';
+const doi2 = '10.1128/aac.01305-10';
 
 // TODO faire un elaticsearch
 describe('test API', () => {
-    it('It should get empty tab because doi not found on database', async () => {
-      const response = await chai.request(server)
-        .get('/graphql?query={getDatasUPW(dois:["Coin Coin"]){doi, is_oa, genre, oa_status}}');
-      response.should.have.status(200);
-      response.body.data.getDatasUPW.should.be.an('array');
-      response.body.data.getDatasUPW.should.eql([]);
-    });
+  it('It should get empty tab because doi not found on database', async () => {
+    const response = await chai.request(server)
+      .get('/graphql?query={getDatasUPW(dois:["Coin Coin"]){doi, is_oa, genre, oa_status}}');
+    response.should.have.status(200);
+    response.body.data.getDatasUPW.should.be.an('array');
+    response.body.data.getDatasUPW.should.eql([]);
   });
 
   describe('get datas UnPayWall with more dois', () => {
     it('It should get 2 datas', async () => {
       const response = await chai.request(server)
-        .get(`/graphql?query={getDatasUPW(dois:["${dataTest1.doi}","${dataTest2.doi}"]){doi, is_oa, genre, oa_status}}`);
+        .get(`/graphql?query={getDatasUPW(dois:["${doi1}","${doi2}"]){doi, is_oa, genre, oa_status}}`);
       response.should.have.status(200);
       response.body.data.getDatasUPW.should.be.a('array');
       response.body.data.getDatasUPW[0].should.have.property('is_oa').eq(true);
@@ -32,7 +31,7 @@ describe('test API', () => {
 
     it('It should get 1 data', async () => {
       const response = await chai.request(server)
-        .get(`/graphql?query={getDatasUPW(dois:["${dataTest1.doi}","Coin Coin"]){doi, is_oa, genre, oa_status}}`);
+        .get(`/graphql?query={getDatasUPW(dois:["${doi1}","Coin Coin"]){doi, is_oa, genre, oa_status}}`);
       response.should.have.status(200);
       response.body.data.getDatasUPW.should.be.a('array');
       response.body.data.getDatasUPW[0].should.have.property('is_oa').eq(true);
