3efd01c892da63aa7791bb4f4d4ca0aaa2a3ac11
wilmouths <wilmouthsteven@gmail.com>
2020-09-22 17:17:34 +0200

    feat: auth in elastic client

--- client/client.js
+++ client/client.js
@@ -1,7 +1,15 @@
 const { Client } = require('@elastic/elasticsearch');
+const { URL } = require('url');
+const { elasticsearch } = require('config');
 
 module.exports = new Client({
-  node: 'http://es01:9200',
+  node: {
+    url: new URL(`http://${elasticsearch.host}:${elasticsearch.port}`),
+    auth: {
+      username: elasticsearch.username,
+      password: elasticsearch.password,
+    },
+  },
   maxRetries: 5,
   requestTimeout: 60000,
   sniffOnStart: true,
