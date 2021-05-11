663c221f00f4cba6a02b13783968be9e67de231f
felixleo22 <leofelixpro22@gmail.com>
2021-05-11 17:35:26 +0200

    jsdoc: createReport and getReport

--- api/services/update/report.js
+++ api/services/update/report.js
@@ -3,17 +3,17 @@ const fs = require('fs-extra');
 const { logger } = require('../../lib/logger');
 
 const reportDir = path.resolve(__dirname, '..', '..', 'out', 'update', 'report');
-const stateDir = path.resolve(__dirname, '..', '..', 'out', 'update', 'state');
 
 const {
-  getState
-} = require('./state')
+  getState,
+} = require('./state');
 
 /**
- * @param {*} fileName - state file name
+ * create report on the folder "out/update/report" on behalf of the date of treatment
+ * @param {String} stateName - state filename
  */
 const createReport = async (stateName) => {
-  let state = await getState(stateName)
+  const state = await getState(stateName);
   const pathfile = path.resolve(reportDir, `${new Date().toISOString().slice(0, 10)}.json`);
   try {
     await fs.writeFile(pathfile, JSON.stringify(state, null, 2));
@@ -23,7 +23,8 @@ const createReport = async (stateName) => {
 };
 
 /**
- * @param {*} fileName - report file name
+ * get report from the folder "out/update/report"
+ * @param {*} fileName - report filename
  * @returns {Object} report
  */
 const getReport = async (fileName) => {
@@ -39,4 +40,4 @@ const getReport = async (fileName) => {
 module.exports = {
   createReport,
   getReport,
-}
+};
