b456f73faaa9b330e81cdb4c847ce0071f83855b
felixleo22 <leofelixpro22@gmail.com>
2021-07-15 13:37:30 +0200

    fix: rename route

--- src/update/routers/status.js
+++ src/update/routers/status.js
@@ -5,6 +5,6 @@ const { getStatus } = require('../bin/status');
 /**
  * @apiSuccess status
  */
-router.get('/update/status', (req, res) => res.status(200).json({ inUpdate: getStatus() }));
+router.get('/status', (req, res) => res.status(200).json({ inUpdate: getStatus() }));
 
 module.exports = router;
