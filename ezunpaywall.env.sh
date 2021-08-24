57a21e3aea1a2eb206cceff9e7dadc663f3f0f49
felixleo22 <leofelixpro22@gmail.com>
2021-08-24 16:33:48 +0200

    feat: fix host

--- ezunpaywall.env.sh
+++ ezunpaywall.env.sh
@@ -7,6 +7,11 @@ LOCAL_ENV_FILE="$SCRIPT_DIR/ezunpaywall.local.env.sh"
 # unpaywall
 export API_KEY_UPW="X-API-KEY"
 
+# clé api pour autoriser les utilisateurs à requêter sur ezunpaywall
+export API_KEY_USERS="[\"user\"]"
+
+export API_KEY_ADMIN="[\"admin\"]"
+
 # mail
 export SMTP_PORT=25
 export NOTIFICATIONS_SENDER='ezunpaywall <ezunpaywall@inist.fr>'
