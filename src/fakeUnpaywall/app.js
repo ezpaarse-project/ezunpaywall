61a7c8565e08018d2fa8e3be221886345b8e47ae
felixleo22 <leofelixpro22@gmail.com>
2021-09-17 10:50:34 +0200

    feat: update fakeUnpaywall for daily and weekly update

--- src/fakeUnpaywall/app.js
+++ src/fakeUnpaywall/app.js
@@ -39,7 +39,7 @@ app.get('/feed/changefiles', async (req, res) => {
 
   const intervals = ['week', 'day']
   if (!intervals.includes(interval)) {
-    return res.status(404).json({ message: `${interval} is not accepted, only week and day are accepted` });
+    return res.status(400).json({ message: `${interval} is not accepted, only week and day are accepted` });
   }
   const file = `changefiles-${interval}.json`;
   res.sendFile(path.resolve(__dirname, 'snapshots', file));
@@ -51,7 +51,7 @@ app.patch('/changefiles', async (req, res) => {
     return res.status(400).json({ message: 'interval expected' });
   }
 
-  const intervals = ['week', 'day']
+  const intervals = ['week', 'day'];
   if (!intervals.includes(interval)) {
     return res.status(404).json({ message: `${interval} is not accepted, only week and day are accepted` });
   }
@@ -76,30 +76,48 @@ app.patch('/changefiles', async (req, res) => {
 
   changefilesPath = path.resolve(__dirname, 'snapshots', `changefiles-${interval}.json`);
 
-
   const now = Date.now();
   const oneDay = 1 * 24 * 60 * 60 * 1000;
   const oneYear = 1 * 24 * 60 * 60 * 1000 * 365;
 
-  changefilesExample.list[0].to_date = new Date(now - (1 * oneDay)).toISOString().slice(0, 10);
-  changefilesExample.list[0].last_modified = new Date(now - (1 * oneDay)).toISOString().slice(0, 19);
-  changefilesExample.list[0].from_date = new Date(now - (8 * oneDay)).toISOString().slice(0, 10);
+  if (interval === 'week') {
+    changefilesExample.list[0].to_date = new Date(now - (1 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[0].last_modified = new Date(now - (1 * oneDay)).toISOString().slice(0, 19);
+    changefilesExample.list[0].from_date = new Date(now - (8 * oneDay)).toISOString().slice(0, 10);
+  
+    changefilesExample.list[1].to_date = new Date(now - (8 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[1].last_modified = new Date(now - (8 * oneDay)).toISOString().slice(0, 19);
+    changefilesExample.list[1].from_date = new Date(now - (15 * oneDay)).toISOString().slice(0, 10);
+  
+    changefilesExample.list[2].to_date = new Date(now - (15 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[2].last_modified = new Date(now - (15 * oneDay)).toISOString().slice(0, 19);
+    changefilesExample.list[2].from_date = new Date(now - (22 * oneDay)).toISOString().slice(0, 10);
+  
+    changefilesExample.list[3].to_date = new Date(now - oneYear - (1 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[3].last_modified = new Date(now - oneYear - (1 * oneDay)).toISOString().slice(0, 19);
+    changefilesExample.list[3].from_date = new Date(now - oneYear - (8 * oneDay)).toISOString().slice(0, 10);
+  
+    changefilesExample.list[4].to_date = new Date(now - oneYear - (8 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[4].last_modified = new Date(now - oneYear - (8 * oneDay)).toISOString().slice(0, 19);
+    changefilesExample.list[4].from_date = new Date(now - oneYear - (15 * oneDay)).toISOString().slice(0, 10);
+  }
 
-  changefilesExample.list[1].to_date = new Date(now - (8 * oneDay)).toISOString().slice(0, 10);
-  changefilesExample.list[1].last_modified = new Date(now - (8 * oneDay)).toISOString().slice(0, 19);
-  changefilesExample.list[1].from_date = new Date(now - (15 * oneDay)).toISOString().slice(0, 10);
-
-  changefilesExample.list[2].to_date = new Date(now - (15 * oneDay)).toISOString().slice(0, 10);
-  changefilesExample.list[2].last_modified = new Date(now - (15 * oneDay)).toISOString().slice(0, 19);
-  changefilesExample.list[2].from_date = new Date(now - (22 * oneDay)).toISOString().slice(0, 10);
-
-  changefilesExample.list[3].to_date = new Date(now - oneYear - (1 * oneDay)).toISOString().slice(0, 10);
-  changefilesExample.list[3].last_modified = new Date(now - oneYear - (1 * oneDay)).toISOString().slice(0, 19);
-  changefilesExample.list[3].from_date = new Date(now - oneYear - (8 * oneDay)).toISOString().slice(0, 10);
-
-  changefilesExample.list[4].to_date = new Date(now - oneYear - (8 * oneDay)).toISOString().slice(0, 10);
-  changefilesExample.list[4].last_modified = new Date(now - oneYear - (8 * oneDay)).toISOString().slice(0, 19);
-  changefilesExample.list[4].from_date = new Date(now - oneYear - (15 * oneDay)).toISOString().slice(0, 10);
+  if (interval === 'day') {
+    changefilesExample.list[0].date = new Date(now - (0 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[0].last_modified = new Date(now - (0 * oneDay)).toISOString().slice(0, 19);
+  
+    changefilesExample.list[1].date = new Date(now - (1 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[1].last_modified = new Date(now - (1 * oneDay)).toISOString().slice(0, 19);
+  
+    changefilesExample.list[2].date = new Date(now - (2 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[2].last_modified = new Date(now - (2 * oneDay)).toISOString().slice(0, 19);
+  
+    changefilesExample.list[3].date = new Date(now - oneYear - (0 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[3].last_modified = new Date(now - oneYear - (0 * oneDay)).toISOString().slice(0, 19);
+  
+    changefilesExample.list[4].date = new Date(now - oneYear - (1 * oneDay)).toISOString().slice(0, 10);
+    changefilesExample.list[4].last_modified = new Date(now - oneYear - (1 * oneDay)).toISOString().slice(0, 19);
+  }
 
   try {
     await fs.writeFile(changefilesPath, JSON.stringify(changefilesExample, null, 2), 'utf8');
