f337e2a6ac9b17f26d85e3929bde7dab96801daf
felixleo22 <leofelixpro22@gmail.com>
2021-11-17 16:10:26 +0100

    feat: mailerservice

---
+++ src/mail/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const fs = require('fs-extra');
+
+const logger = require('./lib/logger');
+
+const { name, version } = require('./package.json');
+
+const routerMail = require('./routers/mail');
+
+const outDir = path.resolve(__dirname, 'out');
+
+fs.ensureDir(path.resolve(outDir));
+fs.ensureDir(path.resolve(outDir, 'logs'));
+
+const app = express();
+
+app.use(cors({
+  origin: '*',
+  method: ['POST'],
+}));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  res.status(200).json({ name, version });
+});
+
+app.use(routerMail);
+
+/* Errors and unknown routes */
+app.use((req, res, next) => res.status(404).json({ message: `Cannot ${req.method} ${req.originalUrl}` }));
+app.use((error, req, res, next) => res.status(500).json({ message: error.message }));
+
+app.listen(8000, () => {
+  logger.info('ezunpaywall auth service listening on 8000');
+});
