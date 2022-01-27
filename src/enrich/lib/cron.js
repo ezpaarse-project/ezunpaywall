451c343bf8b06e1a8a96f97633af43ef0d2f5cb9
felixleo22 <leofelixpro22@gmail.com>
2022-01-27 14:02:30 +0100

    style: syntax error

--- src/enrich/lib/cron.js
+++ src/enrich/lib/cron.js
@@ -10,7 +10,7 @@ const uploadDir = path.resolve(__dirname, '..', 'out', 'upload');
 
 const deleteFilesInDir = async (directory, maxAgeInDays) => {
   const time = 1 * 24 * 60 * 60 * 1000 * maxAgeInDays;
-  const thershold = Date.now() - time;
+  const threshold = Date.now() - time;
 
   let files;
   try {
@@ -23,7 +23,7 @@ const deleteFilesInDir = async (directory, maxAgeInDays) => {
   for (const file of files) {
     try {
       const stat = await fs.stat(path.join(directory, file));
-      if (stat.mtime < thershold) {
+      if (stat.mtime < threshold) {
         await fs.unlink(path.join(directory, file));
       }
     } catch (err) {
