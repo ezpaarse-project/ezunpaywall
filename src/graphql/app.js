734444a605de9047f46ace237e83689697da05ac
felixleo22 <leofelixpro22@gmail.com>
2022-10-10 13:44:37 +0200

    dependencies: remove boom

--- src/graphql/app.js
+++ src/graphql/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const responseTime = require('response-time');
-const boom = require('@hapi/boom');
 
 const auth = require('./middlewares/auth');
 
@@ -13,8 +12,6 @@ const { pingRedis } = require('./lib/service/redis');
 const { pingElastic } = require('./lib/service/elastic');
 
 const schema = require('./graphql');
-
-const isDev = process.env.NODE_ENV === 'development';
 
 const routerPing = require('./routers/ping');
 
@@ -39,16 +36,9 @@ app.use('/graphql', auth, graphqlHTTP({
 }));
 
 /* Errors and unknown routes */
-app.use((req, res, next) => res.status(404).json(boom.notFound(`Cannot ${req.method} ${req.originalUrl}`)));
-app.use((err, req, res, next) => {
-  const error = err.isBoom ? err : boom.boomify(err, { statusCode: err.statusCode });
+app.use((req, res, next) => res.status(404).json({ message: `Cannot ${req.method} ${req.originalUrl}` }));
 
-  if (isDev && error.isServer) {
-    error.output.payload.stack = error.stack;
-  }
-
-  res.status(error.output.statusCode).set(error.output.headers).json(error.output.payload);
-});
+app.use((error, req, res, next) => res.status(500).json({ message: error.message }));
 
 app.listen(3000, () => {
   logger.info('ezunpaywall graphQL API listening on 3000');
