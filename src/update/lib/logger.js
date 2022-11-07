3cf6fd7f5c5dfbada646c04505bbe03012a4b5aa
felixleo22 <leofelixpro22@gmail.com>
2022-11-07 13:43:50 +0100

    fix: error logger

--- src/update/lib/logger.js
+++ src/update/lib/logger.js
@@ -50,7 +50,7 @@ const errorRequest = (err) => {
     params.forEach((param) => {
       url = `${url}${param[0]}=${param[1]}&`;
     });
-    url.substring(0, url.length - 1);
+    url = url.substring(0, url.length - 1);
   }
 
   if (!err?.response) {
