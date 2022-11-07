80eb58676eeaf22725194e672fd9390d53d0fcf9
felixleo22 <leofelixpro22@gmail.com>
2022-11-07 14:38:40 +0100

    fix: error logger for email

--- src/update/lib/logger.js
+++ src/update/lib/logger.js
@@ -41,7 +41,7 @@ const logger = createLogger({
 });
 
 const errorRequest = (err) => {
-  let url = `${err.config.baseURL}${err.config.url}`;
+  let url = `${err?.config?.baseURL}${err?.config?.url}`;
 
   if (err.config.params) {
     let { params } = err.config;
