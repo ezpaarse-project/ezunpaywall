6980b38508925a53891b641e5fd63140633330c0
felixleo22 <leofelixpro22@gmail.com>
2023-01-03 15:17:39 +0100

    feat: health redis, unpaywall, elastic

--- src/health/bin/ping.js
+++ src/health/bin/ping.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const config = require('config');
+const redis = require('redis');
 
 const logger = require('../lib/logger');
 
-async function pingWithTimeout(p1, name, timeout) {
+async function healthWithTimeout(p1, name, timeout) {
   const start = Date.now();
 
   const p2 = new Promise((resolve, reject) => {
@@ -30,41 +31,104 @@ async function pingWithTimeout(p1, name, timeout) {
   };
 }
 
-async function health(name, service) {
+async function health(name, host) {
   let res;
   try {
     res = await axios({
       method: 'GET',
-      url: `${service}/health`,
+      url: `${host}/health`,
     });
   } catch (err) {
-    logger.error(`Cannot request ${name} service at ${service}/health`);
+    logger.error(`Cannot request ${name} service at ${host}/health`);
     return false;
   }
   return res.data;
 }
 
+async function pingWithTimeout(p1, name, timeout) {
+  const start = Date.now();
+
+  const p2 = new Promise((resolve, reject) => {
+    setTimeout(reject, timeout, new Error('time out'));
+  });
+
+  let reply;
+
+  try {
+    reply = await Promise.race([p1, p2]);
+  } catch (err) {
+    return {
+      name, elapsedTime: Date.now() - start, error: err?.message, status: false,
+    };
+  }
+
+  let error;
+  let status = true;
+
+  if (reply !== true) {
+    error = reply;
+    status = false;
+  }
+
+  return {
+    name, elapsedTime: Date.now() - start, error, status,
+  };
+}
+
+async function ping(name, host) {
+  try {
+    await axios({
+      method: 'GET',
+      url: `${host}`,
+    });
+  } catch (err) {
+    logger.error(`Cannot request ${name} service at ${host}`);
+    return err?.message;
+  }
+  return true;
+}
+
+async function pingRedisWithClient() {
+  const redisClient = redis.createClient({
+    host: config.get('redis.host'),
+    port: config.get('redis.port'),
+    password: config.get('redis.password'),
+  });
+
+  try {
+    await redisClient.ping();
+  } catch (err) {
+    logger.error(`Cannot ping ${config.get('redis.host')}:${config.get('redis.port')}`);
+    return err?.message;
+  }
+  return true;
+}
+
 async function pingAll() {
   const graphqlHost = config.get('graphql');
-  const healthGraphql = pingWithTimeout(health('graphql', graphqlHost), 'graphql', 5000);
+  const healthGraphql = healthWithTimeout(health('graphql', graphqlHost), 'graphql', 5000);
 
   const updateHost = config.get('update');
-  const healthUpdate = pingWithTimeout(health('update', updateHost), 'update', 5000);
+  const healthUpdate = healthWithTimeout(health('update', updateHost), 'update', 5000);
 
   const enrichHost = config.get('enrich');
-  const healthEnrich = pingWithTimeout(health('enrich', enrichHost), 'enrich', 5000);
+  const healthEnrich = healthWithTimeout(health('enrich', enrichHost), 'enrich', 5000);
 
   const apikeyHost = config.get('apikey');
-  const healthApikey = pingWithTimeout(health('apikey', apikeyHost), 'apikey', 5000);
+  const healthApikey = healthWithTimeout(health('apikey', apikeyHost), 'apikey', 5000);
 
   const mailHost = config.get('mail');
-  const healthMail = pingWithTimeout(health('mail', mailHost), 'mail', 5000);
+  const healthMail = healthWithTimeout(health('mail', mailHost), 'mail', 5000);
 
-  // TODO elastic
+  const elasticHost = config.get('elastic');
+  const pingElastic = pingWithTimeout(ping('elastic', elasticHost), 'elastic', 5000);
+
+  const unpaywallHost = config.get('unpaywall.host');
+  const pingUnpaywall = pingWithTimeout(ping('unpaywall', unpaywallHost), 'unpaywall', 5000);
+
+  const pingRedis = pingWithTimeout(pingRedisWithClient(), 'redis', 5000);
 
   // TODO redis
-
-  // TODO unpaywall
 
   const result = await Promise.allSettled([
     healthGraphql,
@@ -72,6 +136,9 @@ async function pingAll() {
     healthEnrich,
     healthApikey,
     healthMail,
+    pingElastic,
+    pingUnpaywall,
+    pingRedis,
   ]);
 
   return result;
