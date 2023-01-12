33f306f9c3b08569f55c73b1df1dbf1712e499e3
felixleo22 <leofelixpro22@gmail.com>
2023-01-12 08:18:07 +0100

    feat: use apikey than password redis

--- src/apikey/middlewares/auth.js
+++ src/apikey/middlewares/auth.js
@@ -1,16 +1,15 @@
-const config = require('config');
+const { apikey } = require('config');
 
 /**
- * check the user's api key
  * @param {Object} req - HTTP request
  * @param {Object} res - HTTP response
  * @param {function} next - do the following
  * @returns {Object|function} res or next
  */
 const checkAuth = (req, res, next) => {
-  const apikey = req.get('x-api-key');
+  const key = req.get('x-api-key');
 
-  if (apikey !== config.get('redis.password')) {
+  if (key !== apikey) {
     return res.status(401).json({ message: 'Not authorized' });
   }
 
