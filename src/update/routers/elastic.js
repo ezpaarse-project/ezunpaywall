8be30851ff990fc1a2bc9a90a5dbdbbda3afab14
felixleo22 <leofelixpro22@gmail.com>
2023-02-09 16:14:06 +0100

    fix: add auth middleware

--- src/update/routers/elastic.js
+++ src/update/routers/elastic.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+const checkAuth = require('../middlewares/auth');
+
 const { initAlias } = require('../lib/service/elastic');
 const unpaywallMapping = require('../mapping/unpaywall.json');
 
-router.post('/elastic/alias', async (req, res, next) => {
+router.post('/elastic/alias', checkAuth, async (req, res, next) => {
   try {
     await initAlias('unpaywall', unpaywallMapping, 'upw');
   } catch (err) {
