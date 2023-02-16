74a8a421e6496d8a40abc673e8d4f50aa7f47730
felixleo22 <leofelixpro22@gmail.com>
2023-02-16 13:27:46 +0100

    fix: sendMailContact not async

--- src/mail/lib/routers/mail.js
+++ src/mail/lib/routers/mail.js
@@ -29,7 +29,7 @@ router.post('/contact', checkAuth, async (req, res, next) => {
   }
 
   try {
-    await sendMailContact(email, subject, message);
+    sendMailContact(email, subject, message);
   } catch (err) {
     return next(err);
   }
