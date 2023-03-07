f1f6b20b94035f33d80ccdd30130129e9bcd1868
felixleo22 <leofelixpro22@gmail.com>
2023-03-07 08:27:16 +0100

    feat: responsive status button

--- src/frontend/components/Header.vue
+++ src/frontend/components/Header.vue
@@ -3,53 +3,20 @@
     <v-app-bar-nav-icon dark @click.stop="setDrawer()" />
     <v-toolbar-title> {{ getTitle() }} </v-toolbar-title>
     <v-spacer />
-    <v-menu
-      v-model="help"
-      :close-on-content-click="false"
-      :nudge-width="200"
-      max-width="500"
-      offset-x
-      transition="slide-x-transition"
-    >
-      <template #activator="{ on }">
-        <v-btn class="mr-5" icon v-on="on">
-          <v-icon>mdi-help-circle</v-icon>
-        </v-btn>
-      </template>
-
-      <v-card class="text-justify">
-        <v-card-text>
-          {{ $t('header.help') }}
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            class="body-2"
-            text
-            @click="help = false"
-          >
-            {{ $t('close') }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-menu>
-    <status />
+    <Help />
+    <Status />
   </v-app-bar>
 </template>
 
 <script>
 import Status from '~/components/header/Status.vue'
+import Help from '~/components/header/Help.vue'
 
 export default {
   name: 'Header',
   components: {
-    Status
-  },
-  data: () => {
-    return {
-      help: false
-    }
+    Status,
+    Help
   },
   computed: {
     drawer () {
---
+++ src/frontend/components/header/Help.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-menu
+    v-model="help"
+    :close-on-content-click="false"
+    :nudge-width="200"
+    max-width="500"
+    offset-y
+  >
+    <template #activator="{ on }">
+      <v-btn icon v-on="on">
+        <v-icon>mdi-help-circle</v-icon>
+      </v-btn>
+    </template>
+
+    <v-card class="text-justify">
+      <v-card-text>
+        {{ $t("header.help") }}
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer />
+        <v-btn class="body-2" text @click="help = false">
+          {{ $t("close") }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      help: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
