6f8c5e92a8667c887c2c819a495dbf51e7c48dcd
felixleo22 <leofelixpro22@gmail.com>
2023-08-31 09:57:28 +0200

    fix: uncomment code

--- src/frontend2/plugins/highlight.js
+++ src/frontend2/plugins/highlight.js
@@ -1,10 +1,10 @@
-// import hljs from 'highlight.js/lib/core';
-// import javascript from 'highlight.js/lib/languages/javascript';
-// import highlightJS from '@highlightjs/vue-plugin';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import highlightJS from '@highlightjs/vue-plugin';
 
-// import { defineNuxtPlugin } from '#imports';
+import { defineNuxtPlugin } from '#imports';
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   hljs.registerLanguage('javascript', javascript);
-//   nuxtApp.vueApp.use(highlightJS);
-// });
+export default defineNuxtPlugin((nuxtApp) => {
+  hljs.registerLanguage('javascript', javascript);
+  nuxtApp.vueApp.use(highlightJS);
+});
