519de16b94739841f1f4725ae3fa3753b2436cca
felixleo22 <leofelixpro22@gmail.com>
2023-09-04 15:07:26 +0200

    comment datefns

--- src/frontend/plugins/dateFns.js
+++ src/frontend/plugins/dateFns.js
@@ -1,44 +1,42 @@
-import * as dateFns from 'date-fns';
-import { fr, enGB as en } from 'date-fns/locale';
-import { defineNuxtPlugin } from '#imports';
+// import * as dateFns from 'date-fns';
+// import { fr, enGB as en } from 'date-fns/locale';
+// import { defineNuxtPlugin } from '#imports';
 
-const locales = { fr, en };
+// const locales = { fr, en };
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const angDateFns = { ...dateFns };
+// export default defineNuxtPlugin((nuxtApp) => {
+//   const angDateFns = { ...dateFns };
 
-  const parseISO = (date) => {
-    if (dateFns.parseISO) {
-      return typeof date !== 'string' ? date : dateFns.parseISO(date);
-    }
-    return date;
-  };
+//   const parseISO = (date) => {
+//     if (dateFns.parseISO) {
+//       return typeof date !== 'string' ? date : dateFns.parseISO(date);
+//     }
+//     return date;
+//   };
 
-  angDateFns.format = (date, format = 'PP', options) => {
-    const locale = nuxtApp.$i18n.locale.value;
-    const opts = options || {};
+//   angDateFns.format = (date, format = 'PP', options) => {
+//     const locale = nuxtApp.$i18n.locale.value;
+//     const opts = options || {};
 
-    return dateFns.format(parseISO(date), format, {
-      ...opts,
-      locale: locales[opts.locale || locale],
-    });
-  };
+//     return dateFns.format(parseISO(date), format, {
+//       ...opts,
+//       locale: locales[opts.locale || locale],
+//     });
+//   };
 
-  angDateFns.formatDistanceToNow = (date, options) => {
-    const { locale } = nuxtApp.$config.i18n;
-    const opts = options || {};
+//   angDateFns.formatDistanceToNow = (date, options) => {
+//     const { locale } = nuxtApp.$config.i18n;
+//     const opts = options || {};
 
-    return dateFns.formatDistanceToNow(parseISO(date), {
-      ...opts,
-      locale: locales[opts.locale || locale],
-    });
-  };
+//     return dateFns.formatDistanceToNow(parseISO(date), {
+//       ...opts,
+//       locale: locales[opts.locale || locale],
+//     });
+//   };
 
-  nuxtApp.$dateFns = angDateFns;
-
-  return {
-    provide: {
-      dateFns: angDateFns,
-    },
-  };
-});
+//   return {
+//     provide: {
+//       dateFns: angDateFns,
+//     },
+//   };
+// });
