2d2a8d44e42507c6c6126adc6440f7a40c58637b
felixleo22 <leofelixpro22@gmail.com>
2023-11-21 10:22:47 +0100

    fix: use fr by default

--- src/frontend/config/i18n.js
+++ src/frontend/config/i18n.js
@@ -1,13 +1,18 @@
-import { defineNuxtPlugin } from '#imports';
-
+/* eslint-disable no-undef */
 import en from '@/locales/en.json';
 import fr from '@/locales/fr.json';
 
 export default defineI18nConfig(() => ({
   legacy: false,
-  locale: 'en',
+  locale: 'fr',
+  locales: ['en', 'fr'],
   messages: {
     en,
     fr,
+  },
+  detectBrowserLanguage: {
+    useCookie: true,
+    cookieKey: 'i18n_redirected',
+    redirectOn: 'root',
   },
 }));
