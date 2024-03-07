3f9ed2f0878f2321c139cd79b2d0dd73f8f7f591
felixleo22 <leofelixpro22@gmail.com>
2024-03-07 08:50:39 +0100

    test: add multine count DOI

--- src/graphql/test/countDOI.js
+++ src/graphql/test/countDOI.js
@@ -67,6 +67,44 @@ describe('Test logger of graphql API', () => {
     });
   });
 
+  describe('POST: unpaywall graphql with multine', () => {
+    const req1 = { query: {}, body: {} };
+    const query1 = `
+      { unpaywall(dois: ["1"]) { doi, is_oa } }
+    `;
+
+    const req2 = { query: {}, body: {} };
+    const query2 = `
+      { unpaywall(dois: ["1", "2"]) { doi, is_oa } }
+    `;
+
+    const req3 = { query: {}, body: {} };
+    const query3 = `
+      { unpaywall(dois: []) { doi, is_oa } }
+    `;
+
+    before(() => {
+      req1.body.query = query1;
+      req2.body.query = query2;
+      req3.body.query = query3;
+    });
+
+    it('Should get 1', async () => {
+      const count = getNumberOfDOI(req1);
+      expect(count).eql(1);
+    });
+
+    it('Should get 2', async () => {
+      const count = getNumberOfDOI(req2);
+      expect(count).eql(2);
+    });
+
+    it('Should get 0', async () => {
+      const count = getNumberOfDOI(req3);
+      expect(count).eql(0);
+    });
+  });
+
   describe('POST: unpaywall graphql other syntax', () => {
     const req1 = { query: {}, body: {} };
     const query = 'query ($dois: [ID!]!) { unpaywall(dois:  $dois) { doi, is_oa } }';
