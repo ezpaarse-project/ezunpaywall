e4929a8f5dfaaea0752afbe6ae88c0e1b60b9604
felixleo22 <leofelixpro22@gmail.com>
2024-03-29 10:33:59 +0100

    doc: update jsdoc

--- src/apikey/lib/routers/openapi.js
+++ src/apikey/lib/routers/openapi.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const path = require('path');
 
 /**
- * Route that give the openapi.json file.
+ * Route that give the openapi.yml file.
  */
 router.get('/openapi.yml', (req, res) => res.sendFile(path.resolve(__dirname, '..', '..', 'openapi.yml')));
 
