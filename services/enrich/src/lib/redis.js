f8037e1e203d62b6d8017d65deae6f73b402997b
felixleo22 <leofelixpro22@gmail.com>
2024-11-19 17:28:32 +0100

    fix: update redis client

--- services/enrich/src/lib/redis.js
+++ services/enrich/src/lib/redis.js
@@ -1,9 +1,9 @@
 const { createClient } = require('redis');
 const util = require('util');
 const { redis } = require('config');
-const logger = require('./logger/appLogger');
+const appLogger = require('./logger/appLogger');
 
-const redisClient = createClient({
+let redisClient = createClient({
   legacyMode: true,
   socket: {
     host: redis.host,
@@ -12,23 +12,75 @@ const redisClient = createClient({
   password: redis.password,
 });
 
-//
+function initClient() {
+  try {
+    redisClient = createClient({
+      legacyMode: true,
+      socket: {
+        host: redis.host,
+        port: redis.port,
+      },
+      password: redis.password,
+    });
+    appLogger.info('[redis]: client is created');
+  } catch (err) {
+    appLogger.error(`[redis]: Cannot create redis client - ${err}`);
+    throw err;
+  }
+
+  redisClient.on('connect', () => {
+    appLogger.info('[redis]: redis is connect');
+  });
+
+  redisClient.on('ready', () => {
+    appLogger.info('[redis]: redis is ready');
+  });
+
+  redisClient.on('error', (err) => {
+    appLogger.error(`[redis]: redis in on error [${err}]`);
+  });
+
+  redisClient.on('reconnecting', () => {
+    appLogger.error('[redis]: reconnecting');
+  });
+}
+
 redisClient.get = util.promisify(redisClient.get);
+redisClient.del = util.promisify(redisClient.del);
 redisClient.ping = util.promisify(redisClient.ping);
+redisClient.set = util.promisify(redisClient.set);
+redisClient.keys = util.promisify(redisClient.keys);
+redisClient.flushall = util.promisify(redisClient.flushall);
+
+redisClient.on('connect', () => {
+  appLogger.info('[redis]: redis is connect');
+});
+
+redisClient.on('ready', () => {
+  appLogger.info('[redis]: redis is ready');
+});
+
+redisClient.on('error', (err) => {
+  appLogger.error(`[redis]: redis in on error [${err}]`);
+});
+
+redisClient.on('reconnecting', () => {
+  appLogger.error('[redis]: reconnecting');
+});
 
 /**
- * Ping redis service.
+ * Load the dev apiKeys on redis from apikey-dev.json.
+ * Using for test.
  *
- * @returns {Promise<boolean>} Ping
+ * @returns {Promise<boolean>} ping
  */
 async function pingRedis() {
   try {
     await redisClient.ping();
   } catch (err) {
-    logger.error(`[redis]: Cannot ping ${redis.host}:${redis.port}`, err);
+    appLogger.error(`[redis]: Cannot ping ${redis.host}:${redis.port}`, err);
     return false;
   }
-  logger.info(`[redis]: ping success ${redis.host}:${redis.port}`);
   return true;
 }
 
@@ -36,15 +88,16 @@ async function startConnectionRedis() {
   try {
     await redisClient.connect();
   } catch (err) {
-    logger.error(`[redis]: Cannot start connection ${redis.host}:${redis.port}`, err);
+    appLogger.error(`[redis]: Cannot start connection ${redis.host}:${redis.port}`, err);
     return false;
   }
-  logger.info(`[redis]: Connect success ${redis.host}:${redis.port}`);
+  appLogger.info(`[redis]: Connect success ${redis.host}:${redis.port}`);
   return true;
 }
 
 module.exports = {
   redisClient,
-  startConnectionRedis,
+  initClient,
   pingRedis,
+  startConnectionRedis,
 };
--- services/graphql/src/lib/redis.js
+++ services/graphql/src/lib/redis.js
@@ -1,9 +1,9 @@
 const { createClient } = require('redis');
 const util = require('util');
 const { redis } = require('config');
-const logger = require('./logger/appLogger');
+const appLogger = require('./logger/appLogger');
 
-const redisClient = createClient({
+let redisClient = createClient({
   legacyMode: true,
   socket: {
     host: redis.host,
@@ -12,11 +12,65 @@ const redisClient = createClient({
   password: redis.password,
 });
 
+function initClient() {
+  try {
+    redisClient = createClient({
+      legacyMode: true,
+      socket: {
+        host: redis.host,
+        port: redis.port,
+      },
+      password: redis.password,
+    });
+    appLogger.info('[redis]: client is created');
+  } catch (err) {
+    appLogger.error(`[redis]: Cannot create redis client - ${err}`);
+    throw err;
+  }
+
+  redisClient.on('connect', () => {
+    appLogger.info('[redis]: redis is connect');
+  });
+
+  redisClient.on('ready', () => {
+    appLogger.info('[redis]: redis is ready');
+  });
+
+  redisClient.on('error', (err) => {
+    appLogger.error(`[redis]: redis in on error [${err}]`);
+  });
+
+  redisClient.on('reconnecting', () => {
+    appLogger.error('[redis]: reconnecting');
+  });
+}
+
 redisClient.get = util.promisify(redisClient.get);
+redisClient.del = util.promisify(redisClient.del);
 redisClient.ping = util.promisify(redisClient.ping);
+redisClient.set = util.promisify(redisClient.set);
+redisClient.keys = util.promisify(redisClient.keys);
+redisClient.flushall = util.promisify(redisClient.flushall);
+
+redisClient.on('connect', () => {
+  appLogger.info('[redis]: redis is connect');
+});
+
+redisClient.on('ready', () => {
+  appLogger.info('[redis]: redis is ready');
+});
+
+redisClient.on('error', (err) => {
+  appLogger.error(`[redis]: redis in on error [${err}]`);
+});
+
+redisClient.on('reconnecting', () => {
+  appLogger.error('[redis]: reconnecting');
+});
 
 /**
- * Ping redis service.
+ * Load the dev apiKeys on redis from apikey-dev.json.
+ * Using for test.
  *
  * @returns {Promise<boolean>} ping
  */
@@ -24,7 +78,7 @@ async function pingRedis() {
   try {
     await redisClient.ping();
   } catch (err) {
-    logger.error(`[redis]: Cannot ping ${redis.host}:${redis.port}`, err);
+    appLogger.error(`[redis]: Cannot ping ${redis.host}:${redis.port}`, err);
     return false;
   }
   return true;
@@ -34,15 +88,16 @@ async function startConnectionRedis() {
   try {
     await redisClient.connect();
   } catch (err) {
-    logger.error(`[redis]: Cannot start connection ${redis.host}:${redis.port}`, err);
+    appLogger.error(`[redis]: Cannot start connection ${redis.host}:${redis.port}`, err);
     return false;
   }
-  logger.info(`[redis]: Connect success ${redis.host}:${redis.port}`);
+  appLogger.info(`[redis]: Connect success ${redis.host}:${redis.port}`);
   return true;
 }
 
 module.exports = {
   redisClient,
+  initClient,
   pingRedis,
   startConnectionRedis,
 };
