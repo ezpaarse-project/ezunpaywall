c686e882ab83341265b59487537beafea304f322
felixleo22 <leofelixpro22@gmail.com>
2025-01-27 16:30:24 +0100

    refactor: remove useless route

---
+++ services/enrich/src/controllers/status.js
@@ -0,0 +1,31 @@
+const promiseWithTimeout = require('../lib/ping');
+const { pingRedis } = require('../lib/redis');
+const { pingGraphql } = require('../lib/graphql');
+
+/**
+ * Controller to get status of all services connected to enrich service.
+ *
+ * @param {import('express').Request} req HTTP request.
+ * @param {import('express').Response} res HTTP response.
+ * @param {import('express').NextFunction} next Do the following.
+ */
+async function statusController(req, res, next) {
+  const start = Date.now();
+
+  const p1 = promiseWithTimeout(pingRedis(), 'redis');
+  const p2 = promiseWithTimeout(pingGraphql(), 'graphql');
+
+  let resultPing = await Promise.allSettled([p1, p2]);
+  resultPing = resultPing.map((e) => e.value);
+  const result = {};
+
+  resultPing.forEach((e) => {
+    result[e?.name] = { elapsedTime: e?.elapsedTime, healthy: e?.healthy, error: e?.error };
+  });
+
+  const healthy = resultPing.every((e) => e?.healthy);
+
+  return res.status(200).json({ ...result, elapsedTime: Date.now() - start, healthy });
+}
+
+module.exports = statusController;
