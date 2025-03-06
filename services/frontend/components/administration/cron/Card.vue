451a2f29b7788366e59527843dbdd0270207da21
felixleo22 <leofelixpro22@gmail.com>
2025-03-06 09:51:57 +0100

    style: indentation

--- services/frontend/components/administration/cron/Card.vue
+++ services/frontend/components/administration/cron/Card.vue
@@ -4,21 +4,36 @@
       <v-row class="justify-center align-center">
         {{ props.name }}
         <v-spacer />
-        <v-switch v-model="cronConfig.active" label="Active" color="green-darken-3" hide-details="true"
-          @change="updateActive" />
+        <v-switch
+          v-model="cronConfig.active"
+          label="Active"
+          color="green-darken-3"
+          hide-details="true"
+          @change="updateActive"
+        />
       </v-row>
     </v-card-title>
     <v-list>
-      <v-list-item v-for="(item, index) in configAsArray" :key="index">
+      <v-list-item
+        v-for="(item, index) in configAsArray"
+        :key="index"
+      >
         {{ Object.keys(item)[0] }} : {{ Object.values(item)[0] }}
       </v-list-item>
     </v-list>
     <v-card-actions>
       <v-spacer />
-      <v-btn @click.stop="visible = true">update</v-btn>
+      <v-btn @click.stop="visible = true">
+        update
+      </v-btn>
     </v-card-actions>
   </v-card>
-  <AdministrationCronDialog v-model="visible" :name="'dataUpdate'" :config="configAsArray"  @updated="getCron()" />
+  <AdministrationCronDialog
+    v-model="visible"
+    :name="'dataUpdate'"
+    :config="configAsArray"
+    @updated="getCron()"
+  />
 </template>
 
 <script setup>
@@ -43,15 +58,13 @@ const filteredConfig = computed(() => {
   delete copyCronConfig.name;
   delete copyCronConfig.active;
   return copyCronConfig;
-})
+});
 
-const action = computed(() => {
-  return cronConfig.value.active ? 'start' : 'stop';
-})
+const action = computed(() => (cronConfig.value.active ? 'start' : 'stop'));
 
-const configAsArray = computed(() => {
-  return Object.keys(filteredConfig.value).map(key => ({ [key]: filteredConfig.value[key] }));
-})
+const configAsArray = computed(
+  () => Object.keys(filteredConfig.value).map((key) => ({ [key]: filteredConfig.value[key] })),
+);
 
 async function getCron() {
   let res;
@@ -75,8 +88,8 @@ async function updateActive() {
     await $admin(`/cron/${props.name}/${action.value}`, {
       method: 'POST',
       headers: {
-        'x-api-key': password.value
-      }
+        'x-api-key': password.value,
+      },
     });
   } catch (err) {
     snackStore.error(t('error.cron.active'));
