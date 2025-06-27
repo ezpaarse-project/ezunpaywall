ed30ea7b9bdba68de411782693f6db6564797bbc
felixleo22 <leofelixpro22@gmail.com>
2025-06-27 10:19:25 +0200

    feat: add logo inist-cnrs

--- services/frontend/pages/index.vue
+++ services/frontend/pages/index.vue
@@ -2,5 +2,21 @@
   <section class="ma-3">
     <HomeIntroCard class="my-3" />
     <HomeMetricsCard />
+    <v-row class="align-end justify-center">
+      <v-col cols="auto">
+        <a
+          href="https://inist.fr"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="position-absolute bottom-0 start-50 translate-middle-x my-4"
+        >
+          <img
+            src="/static/images/inist.png"
+            alt="Inist CNRS"
+            style="max-width: 120px; max-height: 120px; width: auto; height: auto;"
+          >
+        </a>
+      </v-col>
+    </v-row>
   </section>
 </template>
