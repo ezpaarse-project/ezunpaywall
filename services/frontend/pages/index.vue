2edee0e116bfb32ea27de8846d9f9f4aa83aa6be
felixleo22 <leofelixpro22@gmail.com>
2025-07-01 10:28:34 +0200

    fix: position of inist-cnrs image

--- services/frontend/pages/index.vue
+++ services/frontend/pages/index.vue
@@ -2,21 +2,26 @@
   <section class="ma-3">
     <HomeIntroCard class="my-3" />
     <HomeMetricsCard />
-    <v-row class="align-end justify-center">
-      <v-col cols="auto">
-        <a
-          href="https://inist.fr"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="position-absolute bottom-0 start-50 translate-middle-x my-4"
-        >
-          <img
-            src="/static/images/inist.png"
-            alt="Inist CNRS"
-            style="max-width: 120px; max-height: 120px; width: auto; height: auto;"
+
+    <div class="position-relative">
+      <v-row
+        justify="center"
+        class="mt-10"
+      >
+        <v-col cols="auto">
+          <a
+            href="https://inist.fr"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-        </a>
-      </v-col>
-    </v-row>
+            <img
+              src="/static/images/inist.png"
+              alt="Inist CNRS"
+              style="max-width: 120px; max-height: 120px;"
+            >
+          </a>
+        </v-col>
+      </v-row>
+    </div>
   </section>
 </template>
