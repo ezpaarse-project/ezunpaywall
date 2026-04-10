c7ecbcc7f30b38e8398bbee4b24213837cfc5048
felixleo22 <leofelixpro22@gmail.com>
2026-04-10 14:58:10 +0200

    fix: remove contact function

--- services/harvester-unpaywall/src/lib/mail.js
+++ services/harvester-unpaywall/src/lib/mail.js
@@ -81,32 +81,6 @@ async function pingSMTP() {
 }
 
 /**
- * Send mail contact.
- *
- * @param {string} email Sender email.
- * @param {string} subject Mail subject.
- * @param {string} message Mail message.
- *
- * @returns {Promise<void>}
- */
-async function contactMail(email, subject, message) {
-  try {
-    await sendMail({
-      from: email,
-      to: notifications.receivers,
-      subject: `[ezUNPAYWALL][${notifications.machine}]: ${subject}`,
-      ...generateMail('contact', {
-        message,
-      }),
-    });
-  } catch (err) {
-    logger.error('[mail]: Cannot send contact mail', err);
-    return;
-  }
-  logger.info('[mail]: contact mail was sent');
-}
-
-/**
  * Sends a mail that inform that an update has started start.
  *
  * @param {string} config Config of mail that content.
@@ -203,7 +177,6 @@ module.exports = {
   generateMail,
   sendMail,
   pingSMTP,
-  contactMail,
   updateStartedMail,
   updateReportMail,
   noChangefileMail,
